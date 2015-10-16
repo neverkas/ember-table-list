fddeb2c1e23e43ddc2ebc4ebdac1c1c15f38bb0c
neverkas <emmanuel.lazarte.011@gmail.com>
2015-10-16 19:05:54 -0300

    Se hizo un refactor del componente y de los templates Se removió el controller que ya no era necesario

--- app/components/table-list.js
+++ app/components/table-list.js
@@ -1,63 +1,68 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	query: null,
 	model: null,
 	columns: null,
-	fields: [],
+	results: null,
+
+	actions:{
+		changeOrder: function(){
+		}
+	},
 
 	didInsertElement: function(){
 		this.findContent();
 	},
 
 	listColumnsTitle: function(){
-		var columns = this.get('columns').split(" ");
-		return columns.map(function(item){
-			var columns 	= item.split(":");
-			var columnTitle = columns[0];
+		if(this.get('columns')){		
+			var columns = this.get('columns').split(" ");
 
-			return columnTitle;
-		});
+			return columns.map(function(item){
+				var columns 	= item.split(":");
+				var columnTitle = columns[0];
 
+				return columnTitle;
+			});
+		}
 	}.property('columns'),
 
 	columnsCount: function(){
 		if(this.get('columns')){
-			return this.get('columns').length;
+			var columns = this.get('columns').split(" ");
+
+			return columns.length;
 		}
 	}.property('columns', 'filterText'),
 
 	contentFilter: function(){
-		var content;
+		var results;
 		var filtered = [];
-		var getData;
 
-		if(this._controller && this._controller.data){
-			content = this._controller.data;
+		if(this.get('results')){
+			results = this.get('results')
 		}
 
 		if(this.get('filterText') && this.get('filterText').length > 0){
 			var regex = new RegExp(this.get('filterText').toLowerCase());
 
-			filtered = content.filter(function(item){
+			filtered = results.filter(function(item){
 				return regex.test(item.get('label').toLowerCase());
 			});
 		}else{
-			filtered = content;
+			filtered = results;
 		}
-		
-		//console.log(filtered);
 
 		return filtered;
 
-	}.property('controller', '_controller.data', 'filterText', 'listColumns'),
+	}.property('filterText', 'results'),
 
 	findContent: function(){
-		var controller = this._controller;
-		var promise = this.store.findAll(this.model);																				
+		var _self 		= this;
+		var promise 	= this.store.findAll(this.model);			
 
 		promise.then(function(response){																								
-			controller.set('data', response);
+			_self.set('results', response)
 		});
 	},
 });
