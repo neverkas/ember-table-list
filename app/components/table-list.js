c203bfd2e4e2ebd34570b8737e1e221b8e3ec374
neverkas <emmanuel.lazarte.011@gmail.com>
2015-10-22 15:11:09 -0300

    Se agregaron condicionales en el componente js para evitar que rompa la aplicación al retornar un error

--- app/components/table-list.js
+++ app/components/table-list.js
@@ -9,55 +9,79 @@ export default Ember.Component.extend({
 
 	actions: {		
 		sortAsc: function(item){
-			this.sortContent(item.data, 'asc');
+			if(item.data){
+				this.sortContent(item.data, 'asc');
+			}
 		},
 
 		sortDesc: function(item){
-			this.sortContent(item.data, 'desc');
+			if(item.data){
+				this.sortContent(item.data, 'desc');
+			}
 		}
 	},
 
 	findContent: function(){
-		this.set('controllerContent', this.get('store').findAll(this.get('modelName')));
+		if(this.get('modelName') && this.get('store')){
+			this.set('controllerContent', this.get('store').findAll(this.get('modelName')));
+		}
 	}.on("init"),
-	
+
 	sortContent: function(columName, columnOrder){
-		this.set('sorting', [columName + ':' + columnOrder]);
+		if(columName && columnOrder){
+			this.set('sorting', [columName + ':' + columnOrder]);
+		}
 	},
 
 	listColumns: function(){
-		if(this.get('columns')){		
+		if(this.get('columns') && typeof this.get('columns') == 'string'){		
 			var columns = this.get('columns').split(" ");
+			var data 	= {};
 
 			return columns.map(function(item){
-				var column 	= item.split(":");
+				var column = [];
 
-				return {'title': column[0], 'data': column[1]};
+				if(item && typeof item == 'string'){
+					column 	= item.split(":");
+				}
+
+				if(typeof column == 'object' && column.length == 2){
+					data = {
+						title: column[0], 
+						data: column[1]
+					};
+				}
+
+				return data;
 			});
 		}
 	}.property('columns'),
 
 	columnsCount: function(){
-		if(this.get('columns')){
-			var columns = this.get('columns').split(" ");
+		var columns;
+
+		if(this.get('columns') && typeof this.get('columns') == 'string'){
+			columns = this.get('columns').split(" ");
 
 			return columns.length;
 		}
 	}.property('columns', 'filterText'),
 
 	contentFilter: function(){
-		var content = [];
-		var filtered = [];
+		var content 	= [];
+		var filtered 	= [];
 
 		if(this.get('content')){
 			content = this.get('content');
 		}
 
 		if(this.get('filterText') && this.get('filterText').length > 0){
-			var regex = new RegExp(this.get('filterText').toLowerCase());
+			var regex = new RegExp(this.get('filterText').toString().toLowerCase());
 
 			filtered = content.filter(function(item){
-				return regex.test(item.get('label').toLowerCase());
+				if(item && item.get('label')){
+					return regex.test(item.get('label').toString().toLowerCase());
+				}
 			});
 		}else{
 			filtered = content;
@@ -68,11 +92,15 @@ export default Ember.Component.extend({
 	}.property('filterText', 'content.[]'),
 
 	highlightText: function(){
-		if(this.get('highlight') == true){		
-			$('table').unhighlight();
+		if(this.get('highlight') && this.get('highlight') == true){		
+			if(this.$().unhighlight){ 
+				this.$('table').unhighlight();
+			}
 
 			if(this.get('filterText') && this.get('filterText').length > 0){
-				$('td').highlight(this.get('filterText'));
+				if(this.$().highlight){ 
+					this.$('td').highlight(this.get('filterText'));
+				}
 			}
 		}
 	}.observes('filterText'),
